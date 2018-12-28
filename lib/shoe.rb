cbe08f92a3b001457d816c8ba2391ccf6de6ab41
Sara Walker <sarawall1187@gmail.com>
2018-12-28 23:20:35 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,6 +6,7 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
+    BRANDS << brand
   end
 
   def cobble
@@ -13,7 +14,5 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
  
- def brand=(brand)
-   BRANDS << brand
- end
+ 
 end
